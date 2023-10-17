f663502a071d2ba56ca09d51e9aea00a30a33502
gunhee98 <rjsgmlaha1@gmail.com>
2023-10-17 15:39:17 +0900

    📝 dcos: 파일 이름 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import { Wrapper } from "./styles/Wrapper";
-
 function App() {
   return (
     <>
       <Wrapper>
         <Home />
-        <Navbar />
       </Wrapper>
     </>
   );
