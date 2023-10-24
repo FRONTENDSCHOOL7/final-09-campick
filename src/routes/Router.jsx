f0171b5621193e5c4ef7092289d32c404052d652
sojinkorea <wkdthwls16@naver.com>
2023-10-24 17:48:02 +0900

    🎨 feat: 캠핑장 피드 컴포넌트

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Signup from "../pages/signup/Signup";
+import ProfileSetup from "../pages/profileSetup/ProfileSetup";
 import LoginEmail from "../pages/loginEmail/LoginEmail";
+import MyCampsiteRegister from "../pages/myCampsiteRegister/MyCampsiteRegister";
 import Homefeed from "../pages/home/Homefeed";
 import SignIn from "../pages/signin/Signin";
 import Profile from "../pages/profile/Profile";
@@ -15,11 +17,12 @@ export default function Router() {
         <Route path="/account/" element={<Outlet />}>
           <Route path="login/" element={<LoginEmail />} />
           <Route path="signup/" element={<Signup />} />
-          <Route path="signup/profileSetup" element={<SignIn />} />
+          <Route path="signup/profileSetup" element={<ProfileSetup />} />
         </Route>
         <Route path="/profile/" element={<Outlet />}>
           <Route path="" element={<Profile />} />
           <Route path=":accountUsername" element={<Profile />} />
+          <Route path="mycampsiteregister" element={<MyCampsiteRegister />} />
         </Route>
       </Routes>
     </BrowserRouter>
