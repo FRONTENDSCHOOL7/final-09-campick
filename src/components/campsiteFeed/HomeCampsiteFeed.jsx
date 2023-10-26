8267a052bd49754d6a57048469025727c7d4004b
HyoEun Cho <chohe3212@naver.com>
2023-10-26 14:01:38 +0900

    💄 캠핑장 리스트 스타일 변경

--- src/components/campsiteFeed/HomeCampsiteFeed.jsx
+++ src/components/campsiteFeed/HomeCampsiteFeed.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import { HomeCampsiteImg, WrapperHomeCampsite } from './HomeCampsiteFeed.style'
-import HomeCampsiteItem from './HomeCampsiteItem'
+import React from "react";
+import {
+  HomeCampSiteList,
+  HomeCampSiteTitle,
+  HomeCampsiteImg,
+  WrapperHomeCampsite,
+} from "./HomeCampsiteFeed.style";
+import HomeCampsiteItem from "./HomeCampsiteItem";
 
 export default function HomeCampsiteFeed() {
   return (
     <WrapperHomeCampsite>
-      <HomeCampsiteItem/>
-      <HomeCampsiteItem/>
-      <HomeCampsiteItem/>
-      <HomeCampsiteItem/>
-      
+      <HomeCampSiteTitle>등록된 모든 캠핑장</HomeCampSiteTitle>
+      <HomeCampSiteList>
+        <HomeCampsiteItem />
+        <HomeCampsiteItem />
+        <HomeCampsiteItem />
+        <HomeCampsiteItem />
+      </HomeCampSiteList>
     </WrapperHomeCampsite>
-  )
+  );
 }
--- src/components/campsiteFeed/HomeCampsiteItem.jsx
+++ src/components/campsiteFeed/HomeCampsiteItem.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { CampSiteItemWrapper, HomeCampSiteName, HomeCampSitePrice, HomeCampsiteImg } from './HomeCampsiteFeed.style'
+import React from "react";
+import {
+  CampSiteItemWrapper,
+  HomeCampSiteName,
+  HomeCampSitePrice,
+  HomeCampsiteImg,
+} from "./HomeCampsiteFeed.style";
 
 export default function HomeCampsiteItem() {
   return (
     <CampSiteItemWrapper>
-      <HomeCampsiteImg/>
+      <HomeCampsiteImg />
       <HomeCampSiteName>캠핑장 이름</HomeCampSiteName>
       <HomeCampSitePrice>가격</HomeCampSitePrice>
     </CampSiteItemWrapper>
-  )
+  );
 }
