ef1360703e36d0ffd7beb6cb45b4e29a38afdb11
sojinkorea <wkdthwls16@naver.com>
2023-10-30 10:15:31 +0900

    🎨 feat: 게시물 상세 페이지 #35

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,24 +5,40 @@ import ProfileSetup from "../pages/profileSetup/ProfileSetup";
 import LoginEmail from "../pages/loginEmail/LoginEmail";
 import MyCampsiteRegister from "../pages/myCampsiteRegister/MyCampsiteRegister";
 import Homefeed from "../pages/home/Homefeed";
-import SignIn from "../pages/signin/Signin";
 import Profile from "../pages/profile/Profile";
+import Followlist from "../pages/follow/Followlist";
+import Login from "../pages/login/Login";
+import Reservation from "../pages/reservation/Reservation";
+import Community from "../pages/community/Community";
+import CommunityPost from "../pages/community/CommunityPost";
+import ViewPost from "../pages/viewPost/ViewPost";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginEmail />} />
+        <Route path="/" element={<Login />} />
         <Route path="/homefeed" element={<Homefeed />} />
+        <Route path="/reservation" element={<Reservation />} />
         <Route path="/account/" element={<Outlet />}>
-          <Route path="login/" element={<LoginEmail />} />
-          <Route path="signup/" element={<Signup />} />
+          <Route path="login" element={<LoginEmail />} />
+          <Route path="signup" element={<Signup />} />
           <Route path="signup/profileSetup" element={<ProfileSetup />} />
         </Route>
         <Route path="/profile/" element={<Outlet />}>
           <Route path="" element={<Profile />} />
-          <Route path=":accountUsername" element={<Profile />} />
-          <Route path="mycampsiteregister" element={<MyCampsiteRegister />} />
+          <Route path=":accountUsername" element={<Outlet />}>
+            <Route path="" element={<Profile />} />
+            <Route path=":follow/" element={<Followlist />} />
+          </Route>
+        </Route>
+        <Route path="/product/" element={<Outlet />}>
+          <Route path="upload/" element={<MyCampsiteRegister />} />
+        </Route>
+        <Route path="/community" element={<Outlet />}>
+          <Route path="" element={<Community />} />
+          <Route path="communitypost" element={<CommunityPost />} />
+          <Route path=":post_id" element={<ViewPost />} />
         </Route>
       </Routes>
     </BrowserRouter>
