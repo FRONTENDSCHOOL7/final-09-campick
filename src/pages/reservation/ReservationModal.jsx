fb82795c7927b6b5e48e88367dcd6736e14b1c2f
sojinkorea <wkdthwls16@naver.com>
2023-11-01 23:40:59 +0900

    🎨 feat: 예약하기 토스트 메세지

--- src/pages/reservation/ReservationModal.jsx
+++ src/pages/reservation/ReservationModal.jsx
@@ -10,8 +10,12 @@ import {
   ProfileAccountname,
   ProductTagWrap,
 } from "./ReservationModal.style";
+import { CompleteToast } from "../../components/toast/Toast";
+
 export default function ReservationModal({ productId }) {
   const [data, setData] = useState("");
+  const [showReservationToast, setShowReservationToast] = useState(false);
+
   useEffect(() => {
     const getProductData = async () => {
       const res = await productDetail(productId);
@@ -20,6 +24,19 @@ export default function ReservationModal({ productId }) {
     getProductData();
   }, [productId]);
   console.log(data);
+
+  const onClick = async e => {
+    e.preventDefault();
+    try {
+      setShowReservationToast(true);
+      setTimeout(() => {
+        setShowReservationToast(false);
+      }, 1000);
+    } catch (error) {
+      console.error("예약 실패");
+    }
+  };
+
   return (
     <ModalWrap>
       <ProductImg src={data && data.itemImage} />
@@ -47,7 +64,10 @@ export default function ReservationModal({ productId }) {
       </div>
       <div style={{ padding: "10px" }}>
         <Submitbutton>캠핑장 문의하기</Submitbutton>
-        <Submitbutton>{data.price}원 캠핑장 예약하기</Submitbutton>
+        <Submitbutton onClick={onClick}>
+          {data.price}원 캠핑장 예약하기
+        </Submitbutton>
+        <CompleteToast showCompleteToast={showReservationToast} text={"예약"} />
       </div>
     </ModalWrap>
   );
