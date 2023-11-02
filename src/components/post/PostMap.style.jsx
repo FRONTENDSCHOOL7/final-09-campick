cec67db74a32c353c20e1e576f05bf407b54a38a
HyoEun Cho <chohe3212@naver.com>
2023-11-02 21:27:13 +0900

    💄 스타일 수정

--- src/components/post/PostMap.style.jsx
+++ src/components/post/PostMap.style.jsx
@@ -2,9 +2,17 @@ import styled from "styled-components";
 
 export const PostMapWrapper = styled.div`
   display: flex;
+  font-weight: 600;
   align-items: center;
   font-size: 13px;
   padding: 7px 0 0 10px;
+  cursor: pointer;
+  transition: 0.3s;
+  transform-origin: left center;
+  &:hover{
+    transform: scale(1.1);
+    transition: 0.3s;
+  }
 `; 
 export const PointBlackIcon = styled.img`
   width : 15px;
