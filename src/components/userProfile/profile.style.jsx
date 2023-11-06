c110f3b62a676d59366d1f7e5b6eff94386cb6e9
sojinjang <wkdthwls16@naver.com>
2023-11-06 16:37:04 +0900

    💄 style: 채팅 이미지 수정

--- src/components/userProfile/profile.style.jsx
+++ src/components/userProfile/profile.style.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import chat from "../../assets/icons/icon-message-circle.svg";
+import chat from "../../assets/icons/chat.svg";
 import share from "../../assets/icons/icon-share.svg";
 export const ProfileWrapper = styled.section`
   background-color: #fff;
