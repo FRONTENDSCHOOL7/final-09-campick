cfbfa2bb9dbf93ff383bac1355f52301ac3e1255
HyoEun Cho <chohe3212@naver.com>
2023-11-07 14:22:30 +0900

    🐛 가로 버그 수정

--- src/styles/Wrapper.jsx
+++ src/styles/Wrapper.jsx
@@ -7,7 +7,6 @@ export const Wrapper = styled.div`
   box-shadow: rgb(226, 226, 226) 0px 0px 10px;
   @media (max-width: 768px) {
     margin: 0 auto;
-    width: 100%;
     max-width: 390px; // 가로 폭이 줄어들 때 Wrapper의 최대 너비를 설정해줄 수 있습니다.
   }
 `;
