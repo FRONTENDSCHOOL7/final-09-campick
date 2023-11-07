4b652132ef72df39ba73bc887e59e0fa30157477
HyoEun Cho <chohe3212@naver.com>
2023-11-07 11:30:37 +0900

    💄 메인화면 반응형 처리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Wrapper } from "./styles/Wrapper";
 import Router from "./routes/Router.jsx";
 import background from "../src/assets/image/background3.png";
-import { styled } from "styled-components";
+import styled from "styled-components"; // 잘못된 import 수정
+
 function App() {
   return (
     <>
-      <div>
-        <Background src={background} />
-      </div>
+      <Background />
       <Wrapper>
         <Router />
       </Wrapper>
@@ -16,8 +15,13 @@ function App() {
 }
 export default App;
 
-const Background = styled.img`
+const Background = styled.div`
   width: 450px;
   height: 555px;
   margin-right: 50px;
+  background: url(${background}) no-repeat center center;
+  background-size: cover;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
